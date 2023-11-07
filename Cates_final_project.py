5f43ca12b0f2d3ed9afac679f396355e944cd67f
Hazelyn <71474732+Hazelyn1@users.noreply.github.com>
2023-11-07 13:57:57 -0500

    Update Cates_final_project.py

--- Cates_final_project.py
+++ Cates_final_project.py
@@ -1,7 +1,7 @@
 #Hazelyn Cates
 #Started 11/6/23
 #EN.605.651.81.FA23
-#This program constructs a phylogenetic tree using Biopython 
+#This program constructs a phylogenetic tree using Biopython package
 
 #DELETE THE FOLLOWING COMMENT, ADD TO REFERENCES IN REPORT!!!!
 #Largely based on this tutorial: https://medium.com/@poudelmohit59/beginners-guide-to-phylogenetic-tree-construction-using-biopython-5accbd8345a2
@@ -9,6 +9,7 @@
 import Bio
 from Bio import SeqIO, AlignIO, Phylo
 from Bio.Phylo.TreeConstruction import DistanceCalculator
+from Bio.Phylo.TreeConstruction import DistanceTreeConstructor
 
 #read in FASTA sequences from file ad put them in an array that can be indexed:
 
@@ -17,7 +18,7 @@ from Bio.Phylo.TreeConstruction import DistanceCalculator
 with open("msa_muscle_output.clw", "r") as msa:
     aligned_seqs = AlignIO.read(msa, "clustal")
 
-#print(aligned_seqs)
+print(aligned_seqs)
 
 #FIX THIS!!! Need to somehow change the names of each sequence b/c it's just the accession numbers rn and I have no idea which is which
 #But if I can't, I'll make sure I specify what's what in my report when I do my results section
@@ -31,4 +32,13 @@ dist_matrix = calc.get_distance(aligned_seqs)
 #print(dist_matrix)
 
 #From here, the NJ and UPGMA algorithms can be used to generate trees and compare the results
+#First, the NJ method: https://homolog.us/Biopython/Bio.Phylo.TreeConstruction.DistanceTreeConstructor.html#content
+construct = DistanceTreeConstructor()
+NJ_tree = construct.nj(dist_matrix)
+#Phylo.draw(NJ_tree)
 
+#Repeat for UPGMA method:
+UPGMA_tree = construct.upgma(dist_matrix)
+
+#print(UPGMA_tree)
+#Phylo.draw(UPGMA_tree)
